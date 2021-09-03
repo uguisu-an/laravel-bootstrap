80d0cc188078445c54fede75ed4d80bd321e19f9
ii masamitsu <ii@entap.co.jp>
2021-09-03 21:04:35 +0900

    日本語ファイルを入れる

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,13 @@ npm install -D prettier @prettier/plugin-php
 
 curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierrc.json > .prettierrc
 curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierignore > .prettierignore
+
+php -r "copy('https://readouble.com/laravel/8.x/ja/install-ja-lang-files.php', 'install-ja-lang.php');"
+php -f install-ja-lang.php
+php -r "unlink('install-ja-lang.php');"
+
+curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/lang/ja.json > resources/lang/ja.json
+
+mkdir src
+mkdir app/Services
+mkdir app/Gateways
