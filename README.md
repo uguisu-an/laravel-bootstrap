e47214513374b514aa512af8f79f0afb630c2e09
ii masamitsu <ii@entap.co.jp>
2021-09-03 19:51:40 +0900

    リソースのダウンロードを追加

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # laravel-bootstrap
+
+## Usage
+
+    curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/bootstrap.sh | bash
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
 php artisan sail:install --with=mariadb,redis,mailhog,selenium
+
 npm install -D prettier @prettier/plugin-php
+
+curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierrc.json >> .prettierrc
+curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierignore >> .prettierignore
