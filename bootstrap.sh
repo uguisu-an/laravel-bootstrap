af9719c3bed31379284d872381ab7643d57a415e
ii masamitsu <ii@entap.co.jp>
2021-09-03 20:53:35 +0900

    上書きしないように調整

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -C
+
 php artisan sail:install --with=mariadb,redis,mailhog,selenium
 
 npm install -D prettier @prettier/plugin-php
 
-curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierrc.json >> .prettierrc
-curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierignore >> .prettierignore
+curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierrc.json > .prettierrc
+curl -s https://raw.githubusercontent.com/uguisu-an/laravel-bootstrap/main/resources/prettierignore > .prettierignore
